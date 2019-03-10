942bd0b79605154a6619c25f442cb790a490b629
dczeeee <wanghaoyu349@126.com>
2019-03-10 17:53:26 +0800

    3/10

--- topK/src/topK/topK.java
+++ topK/src/topK/topK.java
@@ -8,6 +8,8 @@ public class topK {
 		// TODO Auto-generated method stub
 		int[] array = {5,3,8,4,6,9,7,1,2};
 		topk1(array, 5, 0, array.length-1);
+		System.out.println();
+		topk2(array, 5);
 	}
 	//利用快速排序的特点，一次排序后标识前面的都比它大，后面的都比它小，当标识+1等于k的时候输出前k个
 	public static void topk1(int[] array,int k,int left,int right) {
@@ -41,7 +43,52 @@ public class topK {
 		return left;
 	}
 	
+	/*
+	 * 使用最小堆保存数组中最大的K个数，我们取数组前K个数，作为最大的K个数，建最小堆，这个堆中堆顶元素是这个K个数中最小的数，
+	 * 遍历数组，与堆顶元素比较，如果大于堆顶元素，我们就替换堆顶元素，然后调整堆为最小堆。
+	 * 当循环结束时，我们堆中保存的，也就是数组中最大的K个数了
+	 */
 	public static void topk2(int[] array,int k) {
-		//假设数组的前k个就是所要的，然后用之后的n-k个与这k个比较大小
+		int[] res = new int[k];
+		for(int i = 0;i < k;++i) {
+			res[i] = array[i];
+		}
+		createMinHeap(res, k-1);
+		for(int i = k;i < array.length;++i) {
+			if(array[i]>res[0]) {
+				res[0] = array[i];
+				createMinHeap(res, k-1);
+			}
+		}
+		for(int i:res)
+			System.out.print(i+" ");
+	}
+	
+	//建立小根堆
+	private static void createMinHeap(int[] array,int lastIndex) {
+		for(int i = (lastIndex-1)/2;i>=0;--i) {
+			int k = i;
+			while(2*k+1 <= lastIndex) {
+				int bigIndex = 2*k+1;
+				if(bigIndex < lastIndex) {
+					bigIndex++;
+				}
+				if(array[k]>array[bigIndex]) {
+					swap(array,k,bigIndex);
+					k = bigIndex;
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
+	//交换
+	private static void swap(int[] array,int i,int j) {
+		if(i==j)
+			return;
+		array[i] = array[i]+array[j];
+		array[j] = array[i]-array[j];
+		array[i] = array[i]-array[j];
 	}
 }
